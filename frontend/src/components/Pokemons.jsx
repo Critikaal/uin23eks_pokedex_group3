2163c93ceb6d39cadb7a5d0c6092c94e5ccec3c2
Critikaal <93770272+Critikaal@users.noreply.github.com>
2024-05-21 03:10:18 +0200

    la til type knappene sine farger i cssen

--- frontend/src/components/Pokemons.jsx
+++ frontend/src/components/Pokemons.jsx
@@ -83,24 +83,24 @@ export default function Pokemons() {
         </section>
 
         <section id="types">
-          <button> <img src="src/assets/img/type/normal.png" alt="" />Normal</button>
-          <button><img src="src/assets/img/type/fire.png" alt="" />Fire</button>
-          <button><img src="src/assets/img/type/water.png" alt="" />Water</button>
-          <button><img src="src/assets/img/type/grass.png" alt="" />Grass</button>
-          <button><img src="src/assets/img/type/flying.png" alt="" />Flying</button>
-          <button><img src="src/assets/img/type/fighting.png" alt="" />Fighting</button>
-          <button><img src="src/assets/img/type/poison.png" alt="" />Poison</button>
-          <button><img src="src/assets/img/type/electric.png" alt="" />Electric</button>
-          <button><img src="src/assets/img/type/ground.png" alt="" />Ground</button>
-          <button><img src="src/assets/img/type/rock.png" alt="" />Rock</button>
-          <button><img src="src/assets/img/type/psychic.png" alt="" />Psychic</button>
-          <button><img src="src/assets/img/type/ice.png" alt="" />Ice</button>
-          <button><img src="src/assets/img/type/bug.png" alt="" />Bug</button>
-          <button><img src="src/assets/img/type/ghost.png" alt="" />Ghost</button>
-          <button><img src="src/assets/img/type/steel.png" alt="" />Steel</button>
-          <button><img src="src/assets/img/type/dragon.png" alt="" />Dragon</button>
-          <button><img src="src/assets/img/type/dark.png" alt="" />Dark</button>
-          <button><img src="src/assets/img/type/fairy.png" alt="" />Fairy</button>
+          <button className="normal"><img src="src/assets/img/type/normal.png" alt="" />Normal</button>
+          <button className="fire"><img src="src/assets/img/type/fire.png" alt="" />Fire</button>
+          <button className="water"><img src="src/assets/img/type/water.png" alt="" />Water</button>
+          <button className="grass"><img src="src/assets/img/type/grass.png" alt="" />Grass</button>
+          <button className="flying"><img src="src/assets/img/type/flying.png" alt="" />Flying</button>
+          <button className="fighting"><img src="src/assets/img/type/fighting.png" alt="" />Fighting</button>
+          <button className="poison"><img src="src/assets/img/type/poison.png" alt="" />Poison</button>
+          <button className="electric"><img src="src/assets/img/type/electric.png" alt="" />Electric</button>
+          <button className="ground"><img src="src/assets/img/type/ground.png" alt="" />Ground</button>
+          <button className="rock"><img src="src/assets/img/type/rock.png" alt="" />Rock</button>
+          <button className="psychic"><img src="src/assets/img/type/psychic.png" alt="" />Psychic</button>
+          <button className="ice"><img src="src/assets/img/type/ice.png" alt="" />Ice</button>
+          <button className="bug"><img src="src/assets/img/type/bug.png" alt="" />Bug</button>
+          <button className="ghost"><img src="src/assets/img/type/ghost.png" alt="" />Ghost</button>
+          <button className="steel"><img src="src/assets/img/type/steel.png" alt="" />Steel</button>
+          <button className="dragon"><img src="src/assets/img/type/dragon.png" alt="" />Dragon</button>
+          <button className="dark"><img src="src/assets/img/type/dark.png" alt="" />Dark</button>
+          <button className="fairy"><img src="src/assets/img/type/fairy.png" alt="" />Fairy</button>
         </section>
       </main>
       </>
